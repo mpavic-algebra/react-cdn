aaf7135c589a3664fcf2d7b36b00a00778d72a02
Matej Pavic <matej@Matejs-MacBook-Pro.local>
2023-08-31 19:38:49 +0200

    Add JSX example

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const rootNode = document.getElementById("root");
 const root = ReactDOM.createRoot(rootNode)
 
 function App() {
-    // return React.createElement("h1", null, "Hello React")
-    return React.createElement("div", null, 
-        React.createElement("h1", null, "Hello React"),
-        //ovdje ubaciti paragraf
-        React.createElement("p", null, "Nekakav Paragraf"),
-        React.createElement("ul", null, 
-            React.createElement("li", null, "Subtitle 1"),
-            React.createElement("li", null, "Subtitle 2"),
-            React.createElement("li", null, "Subtitle 3"),
-        )
+    return (
+        <>
+            <h1>Pozdrav Algebra iz JSX-a</h1>
+            <Paragraf />
+        </>
+    )
+}
+
+function Paragraf() {
+    return (
+        <>
+            <p>Nekakav paragraf</p>
+        </>
     )
 }
 // const virtualDOM = App()
